1b80f8ae539250d93933cf3f3fcadda995231430
shifink <muhammadshifin07@gmail.com>
2024-03-01 12:47:53 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:api_dio/view/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(home: DioHome(),);
+  }
+}
---
+++ lib/utils/consant.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+
+class MyColors{
+
+  static const Color bgColor =Color.fromARGB(249, 249, 186, 186);
+  static const Color prColor =Color.fromRGBO(49, 72, 252, 0.31);
+  static const Color scColor =Color.fromARGB(244, 164, 5, 37);
+
+}
